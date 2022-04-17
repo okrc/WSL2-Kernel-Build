655d0f5d76823df6771cbb506fb410fddbbd9c2d
okrc <mail@zdev.me>
2022-04-17 21:58:33 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# WSL2-Kernel-Build
+
+[![License][1]][LICENSE]
+[![BuildKernel][2]][3]
+[![Download][4]][RELEASE]
+[![Release][5]][RELEASE]
+[![ReleaseDate][7]][RELEASE]
+
+## Build & install
+
+### **Docker build**
+
+``` sh
+docker-compose up
+docker-compose down
+```
+
+### **WSLCONFIG**
+
+``` conf
+[wsl2]
+kernel=C:\\Users\\okrc\\bzImage     # your path
+```
+
+[1]: https://img.shields.io/github/license/okrc/WSL2-Kernel-Build
+[2]: https://img.shields.io/github/workflow/status/okrc/WSL2-Kernel-Build/Build%20kernel?label=Build%20kernel
+[3]: https://github.com/okrc/WSL2-Kernel-Build/actions/workflows/build-kernel.yml
+[4]: https://img.shields.io/github/downloads/okrc/WSL2-Kernel-Build/total
+[5]: https://img.shields.io/github/v/release/okrc/WSL2-Kernel-Build?display_name=release&sort=date
+[6]: https://img.shields.io/github/v/release/okrc/WSL2-Kernel-Build?display_name=release&include_prereleases&label=pre-release&sort=date
+[7]: https://img.shields.io/github/release-date/okrc/WSL2-Kernel-Build
+[8]: https://img.shields.io/github/release-date-pre/okrc/WSL2-Kernel-Build?label=pre-release-date
+
+[LICENSE]: LICENSE
+[RELEASE]: https://github.com/okrc/WSL2-Kernel-Build/releases
+[WSL2-Linux-Kernel REPO]: https://github.com/microsoft/WSL2-Linux-Kernel
+[The-Linux-Kernel REPO]: https://www.kernel.org
---
+++ build-kernel.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+set -e
+
+CONFIG_BRA=linux-msft-wsl-5.10.y
+
+apt-get update && apt-get install --no-install-recommends --yes curl ca-certificates xz-utils make flex bison libssl-dev libelf-dev bc python3-minimal dwarves \
+    gcc-x86-64-linux-gnu gcc-aarch64-linux-gnu
+
+if [ ! -f 'kernel.tar.xz' ]; then
+    KERNEL_URL=$(curl --silent https://www.kernel.org | sed -n '/<td id="latest_link">/,/<\/td>/p' | grep -Po 'http[^"]+')
+    curl -C- -sL ${KERNEL_URL} -o kernel.tar.xz
+fi
+
+build_amd64_kernel() {
+    TMPDIR=$(mktemp -d)
+    tar xf kernel.tar.xz --strip-components=1 -C $TMPDIR
+    curl -s "https://raw.githubusercontent.com/microsoft/WSL2-Linux-Kernel/${CONFIG_BRA}/Microsoft/config-wsl" |
+        sed -e 's/^[[:space:]]*#\?[[:space:]]*\(.*CONFIG_BLK_DEV_THROTTLING\b\).*/\1=y/g' \
+            -e 's/^[[:space:]]*#\?[[:space:]]*\(.*CONFIG_NFT_\(COUNTER\|COMPAT\|NAT\)\b\).*/\1=y/g' \
+            >$TMPDIR/.config
+    (cd $TMPDIR && make -j$(nproc) ARCH=x86_64 CROSS_COMPILE=x86_64-linux-gnu-)
+    cp $TMPDIR/arch/x86/boot/bzImage ./wsl2-kernel-amd64
+    sha256sum wsl2-kernel-amd64 >./wsl2-kernel-amd64.sha256
+    rm -rf $TMPDIR
+}
+
+build_arm64_kernel() {
+    TMPDIR=$(mktemp -d)
+    tar xf kernel.tar.xz --strip-components=1 -C $TMPDIR
+    curl -s "https://raw.githubusercontent.com/microsoft/WSL2-Linux-Kernel/${CONFIG_BRA}/Microsoft/config-wsl-arm64" |
+        sed -e 's/^[[:space:]]*#\?[[:space:]]*\(.*CONFIG_BLK_DEV_THROTTLING\b\).*/\1=y/g' \
+            -e 's/^[[:space:]]*#\?[[:space:]]*\(.*CONFIG_NFT_\(COUNTER\|COMPAT\|NAT\)\b\).*/\1=y/g' \
+            >$TMPDIR/.config
+    (cd $TMPDIR && make -j$(nproc) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu-)
+    cp $TMPDIR/arch/arm64/boot/Image ./wsl2-kernel-arm64
+    sha256sum wsl2-kernel-arm64 >./wsl2-kernel-arm64.sha256
+    rm -rf $TMPDIR
+}
+
+build_amd64_kernel
+build_arm64_kernel

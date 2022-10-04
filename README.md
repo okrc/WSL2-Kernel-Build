c370480b44c1d830469dbe008ca3dc9ec3619ab4
okrc <okrc@hexo.dev>
2022-10-04 22:56:28 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# WSL2-Kernel-Build
+
+[![License][1]][LICENSE]
+[![BuildKernel][2]][3]
+[![Release][4]][RELEASE]
+[![PreRelease][5]][PRERELEASE]
+
+## Build & install
+
+### **Docker build**
+
+``` sh
+docker run --rm --volume ${PWD}:/prefix --workdir /prefix debian:testing-slim ./build-kernel.sh
+```
+
+### **WSLCONFIG**
+
+``` conf
+[wsl2]
+kernel=C:\\Users\\okrc\\bzImage     # your path
+```
+
+[1]: https://img.shields.io/github/license/okrc/WSL2-Kernel-Build
+[2]: https://img.shields.io/github/workflow/status/okrc/WSL2-Kernel-Build/Build%20kernel?label=Build%20kernel
+[3]: https://github.com/okrc/WSL2-Kernel-Build/actions/workflows/build-kernel.yml
+[4]: https://img.shields.io/github/v/release/okrc/WSL2-Kernel-Build?display_name=release&sort=date
+[5]: https://img.shields.io/github/v/release/okrc/WSL2-Kernel-Build?display_name=release&color=teal&include_prereleases&label=pre-release&sort=date
+
+[LICENSE]: LICENSE
+[RELEASE]: https://github.com/okrc/WSL2-Kernel-Build/releases/latest
+[PRERELEASE]: https://github.com/okrc/WSL2-Kernel-Build/releases
+[WSL2-Linux-Kernel REPO]: https://github.com/microsoft/WSL2-Linux-Kernel
+[The-Linux-Kernel REPO]: https://www.kernel.org
---
+++ build-kernel.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+set -e
+
+apt-get update && apt-get install --no-install-recommends --yes curl ca-certificates xz-utils make flex bison libssl-dev libelf-dev bc python3-minimal dwarves \
+    gcc-x86-64-linux-gnu gcc-aarch64-linux-gnu
+
+if [ ! -f 'kernel.tar.xz' ]; then
+    KERNEL_URL=https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.0.tar.xz
+    curl -C- -sL ${KERNEL_URL} -o kernel.tar.xz
+fi
+
+TMPDIR=$(mktemp -d)
+tar xf kernel.tar.xz --strip-components=1 -C $TMPDIR
+curl --silent --remote-name-all https://raw.githubusercontent.com/microsoft/WSL2-Linux-Kernel/linux-msft-wsl-5.15.y/Microsoft/config-wsl{,-arm64}
+
+build_config() {
+    if [ -f "$1" ]; then
+        sed -e 's/^#[[:space:]]\(CONFIG_BLK_DEV_THROTTLING\b\).*/\1=y/g' \
+            -e 's/^#[[:space:]]\(CONFIG_IPV6_MULTIPLE_TABLES\b\).*/\1=y/g' \
+            -e 's/^#[[:space:]]\(CONFIG_NFT_FIB_IPV4\b\).*/\1=y/g' \
+            -e 's/^#[[:space:]]\(CONFIG_NFT_FIB_IPV6\b\).*/\1=y/g' \
+            -e "w $TMPDIR/.config" \
+            "$1" >/dev/null
+    fi
+}
+
+build_amd64_kernel() {
+    build_config config-wsl
+    (cd $TMPDIR && make -j$(nproc) ARCH=x86_64 CROSS_COMPILE=x86_64-linux-gnu-)
+    cp $TMPDIR/arch/x86/boot/bzImage ./wsl2-kernel-amd64
+    sha256sum wsl2-kernel-amd64 >./wsl2-kernel-amd64.sha256
+}
+
+build_arm64_kernel() {
+    build_config config-wsl-arm64
+    (cd $TMPDIR && make -j$(nproc) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu-)
+    cp $TMPDIR/arch/arm64/boot/Image ./wsl2-kernel-arm64
+    sha256sum wsl2-kernel-arm64 >./wsl2-kernel-arm64.sha256
+}
+
+build_amd64_kernel
+build_arm64_kernel
+
+rm -rf $TMPDIR config-wsl{,-arm64}

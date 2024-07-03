be51ac1c77ea652566723712711aafbb3cd06a21
okrc <okrc@hexo.dev>
2024-07-04 07:52:22 +0800

    Update build-kernel.sh

--- build-kernel.sh
+++ build-kernel.sh
@@ -33,10 +33,10 @@ build_amd64_kernel() {
             sh <(curl -fsSL https://sh.rustup.rs) --quiet -y --default-toolchain $(scripts/min-tool-version.sh rustc) --profile minimal --component rust-src
             . "$HOME/.cargo/env"
             cargo install --locked --version $(scripts/min-tool-version.sh bindgen) bindgen-cli
-            make LLVM=1 rustavailable
+            make LLVM=-${LLVM_VERSION} rustavailable
         fi
 
-        make -j$(nproc) ARCH=x86_64 LLVM=1
+        make -j$(nproc) ARCH=x86_64 LLVM=-${LLVM_VERSION}
     )
     cp $TMPDIR/arch/x86/boot/bzImage ./wsl2-kernel-amd64
     sha256sum wsl2-kernel-amd64 >./wsl2-kernel-amd64.sha256
@@ -44,7 +44,7 @@ build_amd64_kernel() {
 
 build_arm64_kernel() {
     build_config config-wsl-arm64
-    (cd $TMPDIR && make -j$(nproc) ARCH=arm64 LLVM=1)
+    (cd $TMPDIR && make -j$(nproc) ARCH=arm64 LLVM=-${LLVM_VERSION})
     cp $TMPDIR/arch/arm64/boot/Image ./wsl2-kernel-arm64
     sha256sum wsl2-kernel-arm64 >./wsl2-kernel-arm64.sha256
 }
